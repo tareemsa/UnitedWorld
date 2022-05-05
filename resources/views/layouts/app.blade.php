74a73cd8050d148f45ab9987cc6fe9cf92b05376
abdallah97sasa <abdallahsasa@github.com>
2022-05-05 15:04:59 +0300

    icon2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 
 
     <!-- favicon links -->
-    <link rel="shortcut icon" type="image/png" href="front/images/header/logo.png" />
+    <link rel="shortcut icon" type="image/png" href="front/images/header/favicon.png" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
